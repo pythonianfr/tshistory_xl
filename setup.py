c77225cc70bc045c0e4d8ad791ee36660025fbef
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2021-09-21 18:48:16 +0200

    pkg: prepare 0.2.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.name == 'nt':
 
 
 setup(name='tshistory_xl',
-      version='0.2.0',
+      version='0.2.1',
       author='Pythonian',
       author_email='arnaud.campeas@pythonian.fr, aurelien.campeas@pythonian.fr',
       description='Light client for excel/tshistory',
